ad5febd51362e27483ea268222c2f26759ceb864
Tamerlan Gudabyev <tamerlanalbehairy@gmail.com>
2021-06-22 18:06:27 +0600

    feat: database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
+import { DatabaseModule } from './database/database.module';
 import * as Joi from '@hapi/joi';
 
 @Module({
@@ -16,6 +17,7 @@ import * as Joi from '@hapi/joi';
         PORT: Joi.number(),
       }),
     }),
+    DatabaseModule,
   ],
   controllers: [],
   providers: [],
