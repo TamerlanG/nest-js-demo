411ef67bf0606ed1f4bc3581f581c9583fb8db2f
Tamerlan Gudabyev <tamerlanalbehairy@gmail.com>
2021-06-23 16:54:25 +0600

    feat(utils): exclude null interceptor

---
+++ src/utils/recursivelyStripNullValues.ts
@@ -0,0 +1,23 @@
+function recursivelyStripNullValues(value: unknown): unknown {
+  if (Array.isArray(value)) {
+    return value.map(recursivelyStripNullValues);
+  }
+  if (value !== null && typeof value === 'object') {
+    return Object.fromEntries(
+      Object.entries(value).map(([key, value]) => [
+        key,
+        recursivelyStripNullValues(value),
+      ]),
+    );
+  }
+
+  if (value instanceof Date) {
+    return value;
+  }
+
+  if (value !== null) {
+    return value;
+  }
+}
+
+export default recursivelyStripNullValues;
