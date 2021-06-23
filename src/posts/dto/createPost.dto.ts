34575b9974749dfdc2400384f71f89c6ea352bc4
Tamerlan Gudabyev <tamerlanalbehairy@gmail.com>
2021-06-23 16:17:37 +0600

    feat(post): validate dto

--- src/posts/dto/createPost.dto.ts
+++ src/posts/dto/createPost.dto.ts
@@ -1,5 +1,12 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
   content: string;
+
+  @IsString()
+  @IsNotEmpty()
   title: string;
 }
 
