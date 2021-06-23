36bbefc40935370f28a7381c6cdf721ed2b3dd7a
Tamerlan Gudabyev <tamerlanalbehairy@gmail.com>
2021-06-23 16:11:36 +0600

    feat(authentication): validate inputs

--- src/authentication/dto/login.dto.ts
+++ src/authentication/dto/login.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsString, IsNotEmpty, MinLength } from 'class-validator';
+
 export class LoginDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(7)
   password: string;
 }
 
