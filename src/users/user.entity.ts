63720c06af52ce28322000683608b58e291e253f
Tamerlan Gudabyev <tamerlanalbehairy@gmail.com>
2021-06-24 14:21:02 +0600

    feat(user): connect user to address

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,5 +1,14 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { Exclude } from 'class-transformer';
+import Address from './address.entity';
+import Post from 'src/posts/post.entity';
 
 @Entity()
 class User {
@@ -15,6 +24,16 @@ class User {
   @Column()
   @Exclude()
   public password: string;
+
+  @OneToOne(() => Address, {
+    eager: true,
+    cascade: true,
+  })
+  @JoinColumn()
+  public address: Address;
+
+  @OneToMany(() => Post, (post: Post) => post.author)
+  public posts: Post[];
 }
 
 export default User;
